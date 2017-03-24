add309cbf076420bd13a329b539d54d57e7875a0
Matthew Scharley <matt.scharley@gmail.com>
2017-03-24 16:21:25 +1100

    alacritty: Initial file

---
+++ alacritty.rb
@@ -0,0 +1,34 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Alacritty < Formula
+  desc "A cross-platform, GPU-accelerated terminal emulator"
+  homepage "https://github.com/jwilm/alacritty"
+  url "mscharley/homebrew"
+  version ""
+  sha256 ""
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test alacritty`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
